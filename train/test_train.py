4b2fd36dd1d71ecc502ebc56d181227309a948f6
pierreglerant <154981553+pierreglerant@users.noreply.github.com>
2024-05-08 15:10:11 +0100

    feat: changes 2

--- train/test_train.py
+++ train/test_train.py
@@ -1,14 +1,18 @@
 from unittest import TestCase
 import pandas as pd
+import train
 
-# class TestTrain(TestCase):
+class TestTrain(TestCase):
 
-#     def test_clean_data(self):
-#         df = pd.DataFrame({
-#             'survived':[1,0,1],
-#             'pclass': [1,2,3],
-#             'sex':['male','female','male']
-#             'age': [20,30,40]
-#         })
+    def test_clean_data(self):
+        df = pd.DataFrame({
+            'survived':[1,0,1],
+            'pclass': [1,2,3],
+            'sex':['male','female','male'],
+            'age': [20,30,40]
+        })
 
-#         result_df = clean_data
+        result_df = train.clean_data(df)
+
+        self.assertEqual(result_df.shape[0])
+        self.assertEqual(result_df['sex'][0])
